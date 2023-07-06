20187be89291c42dea8f042ade4a654aafc1c4e6
VishalSingu <137383954+vishalsingu78@users.noreply.github.com>
2023-07-06 16:34:59 +0530

    Add files via upload

---
+++ tab.js
@@ -0,0 +1,18 @@
+function abouts(){
+    document.getElementById("detailspaste").textContent=`Varanasi is one of the oldest living cities in the world. Its Prominence in Hindu mythology is virtually unrevealed. Mark Twain, the English author and literature, who was enthralled by the legend and sanctity of Benaras, once wrote, ''Benaras is older than history, older than tradition, older even than legend and looks twice as old as all of them put together". `;
+    document.getElementById("btn1").style.backgroundColor="white";
+    document.getElementById("btn2").style.backgroundColor="rgb(193, 204, 204)";
+    document.getElementById("btn3").style.backgroundColor="rgb(193, 204, 204)";
+}
+function visit(){
+    document.getElementById("detailspaste").textContent=`October to March is the best time to visit Varanasi because most of the fairs here are held during this time of the year. Festivities begin with Diwali and continue to Dev Diwali celebrated on the 15th day from Diwali. In between, there is also Annakut. During this time, the ghats are lit with lights and diyas. Earthen lamps adorn the staircase of the ghats and are also afloat in the river. Firecrackers burn through the night, and it's a sight no one should miss. `;
+    document.getElementById("btn1").style.backgroundColor="rgb(193, 204, 204)";
+    document.getElementById("btn2").style.backgroundColor="white";
+    document.getElementById("btn3").style.backgroundColor="rgb(193, 204, 204)";
+}
+function action(){
+    document.getElementById("detailspaste").textContent=`When visiting Varanasi, one comes across plenty of ghats, but among them, Dashashwamedh Ghat is said to be one of the oldest and most important. This ghat, leading to the Ganges, is located close to the famous old Vishwanath temple in Kashi (today’s Banaras). Another famous attraction is River Ganges which is the holiest river by the Hindus and many more. `;
+    document.getElementById("btn3").style.backgroundColor="white";
+    document.getElementById("btn1").style.backgroundColor="rgb(193, 204, 204)";
+    document.getElementById("btn2").style.backgroundColor="rgb(193, 204, 204)";
+}
